655823807f5fcd3494619d0940c3a483383ee0e0
Krish Koria <krishkoria2004@gmail.com>
2025-02-08 17:02:13 +0000

    add map and mapb commands to retrieve and display location data

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -12,10 +14,28 @@ var commands map[string]cliCommand
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*config) error
+}
+
+type config struct {
+	Next string
+	Previous string
+}
+
+type locationResponse struct {
+	Results []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+	Next string `json:"next"`
+	Previous string `json:"previous"`
 }
 
 func main() {
+	cfg := &config{
+		Next: "https://pokeapi.co/api/v2/location-area?offset=0&limit=20",
+		Previous: "",
+	}
 	commands = map[string]cliCommand{
 		"exit": {
 			name:        "exit",
@@ -26,6 +46,16 @@ func main() {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Displays the map",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Displays the previous map",
+			callback:    commandMapb,
 		},
 	}
 
@@ -40,7 +70,7 @@ func main() {
 		words := cleanInput(text)
 		if len(words) > 0 {
 			if command, ok := commands[words[0]]; ok {
-				err := command.callback()
+				err := command.callback(cfg)
 				if err != nil {
 					fmt.Println("Error executing command:", err)
 				}
@@ -59,17 +89,59 @@ func cleanInput(text string) []string {
 	return words
 }
 
-func commandExit() error {
+func commandExit(cfg *config) error {
     fmt.Println("Closing the Pokedex... Goodbye!")
     os.Exit(0)
     return nil
 }
 
-func commandHelp() error {
+func commandHelp(cfg *config) error {
     fmt.Println("Welcome to the Pokedex!")
     fmt.Println("Usage: \n\n\n")
     for _, cmd := range commands {
         fmt.Printf("%s: %s\n", cmd.name, cmd.description)
     }
     return nil
+}
+
+func commandMap(cfg *config) error {
+	if cfg.Next == "" {
+		return fmt.Errorf("no more locations to display")
+	}
+	res, err := http.Get(cfg.Next)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	var loc locationResponse
+	if err := json.NewDecoder(res.Body).Decode(&loc); err != nil {
+		return err
+	}
+	for _, loc := range loc.Results {
+		fmt.Println(loc.Name)
+	}
+	cfg.Next = loc.Next
+	cfg.Previous = loc.Previous
+	return nil
+}
+
+func commandMapb(cfg *config) error {
+	if cfg.Previous == "" {
+		return fmt.Errorf("no more locations to display")
+	}
+	res, err := http.Get(cfg.Previous)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	var loc locationResponse
+	if err := json.NewDecoder(res.Body).Decode(&loc); err != nil {
+		return err
+	}
+	for _, loc := range loc.Results {
+		fmt.Println(loc.Name)
+	}
+	cfg.Next = loc.Next
+	cfg.Previous = loc.Previous
+	return nil
 }
